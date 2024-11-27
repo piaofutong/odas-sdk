59d47c1f502c51ba5ecfac5be99b4e2e79db988e
zzj <1844066417@qq.com>
2024-11-27 17:09:03 +0800

    添加终端验证按景区分组的列表数据

--- odas/tourist/inout_summary.go
+++ odas/tourist/inout_summary.go
@@ -8,11 +8,50 @@ import (
 )
 
 type SummaryReq struct {
-	Start   string `json:"start"`
-	End     string `json:"end"`
-	Sid     int    `json:"sid"`
-	Gid     int    `json:"gid"`
-	NoAmend bool   `json:"noAmend"`
+	Start    string `json:"start"`
+	End      string `json:"end"`
+	Sid      int    `json:"sid"`
+	Gid      int    `json:"gid"`
+	NoAmend  bool   `json:"noAmend"`
+	DateType int    `json:"dateType"`
+}
+
+type SummaryReqOptions func(options *SummaryReq)
+
+func WithDateType(dateType int) SummaryReqOptions {
+	return func(options *SummaryReq) {
+		options.DateType = dateType
+	}
+}
+
+func WithNoAmend() SummaryReqOptions {
+	return func(options *SummaryReq) {
+		options.NoAmend = true
+	}
+}
+
+func WithStart(start string) SummaryReqOptions {
+	return func(options *SummaryReq) {
+		options.Start = start
+	}
+}
+
+func WithEnd(end string) SummaryReqOptions {
+	return func(options *SummaryReq) {
+		options.End = end
+	}
+}
+
+func WithSid(sid int) SummaryReqOptions {
+	return func(options *SummaryReq) {
+		options.Sid = sid
+	}
+}
+
+func WithGid(gid int) SummaryReqOptions {
+	return func(options *SummaryReq) {
+		options.Gid = gid
+	}
 }
 
 func (s SummaryReq) Api() string {
@@ -31,6 +70,9 @@ func (s SummaryReq) Api() string {
 	}
 	if s.NoAmend {
 		params.Add("noAmend", strconv.FormatBool(s.NoAmend))
+	}
+	if s.DateType > 0 {
+		params.Add("dateType", strconv.Itoa(s.DateType))
 	}
 	return fmt.Sprintf("/v4/tourist/inout/summary?%s", params.Encode())
 }
@@ -51,14 +93,12 @@ func (s SummaryReq) AuthRequired() bool {
 	return true
 }
 
-func NewSummaryReq(start, end string, sid, gid int, noAmend bool) *SummaryReq {
-	return &SummaryReq{
-		Start:   start,
-		End:     end,
-		Sid:     sid,
-		Gid:     gid,
-		NoAmend: noAmend,
+func NewSummaryReq(opt ...SummaryReqOptions) *SummaryReq {
+	req := &SummaryReq{}
+	for _, options := range opt {
+		options(req)
 	}
+	return req
 }
 
 type InoutSummaryResponse struct {
